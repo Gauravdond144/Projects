d40214fe3ee593f8c87d2959abd5c8d6c5f00bf0
gauravd_144 <gauravdond1234@gmail.com>
2024-06-23 09:26:22 +0530

    changes in todoapp

--- todoapp/src/App.js
+++ todoapp/src/App.js
@@ -1,24 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Heading from "./Components/Heading";
+import Inputodo from "./Components/Inputodo";
+import { useReducer } from "react";
+import TodoItems from "./Components/TodoItems";
+import MyContext from "./Components/MyContext";
+
+function todoReducer(currentTodo, todoaction) {
+  let newTodoItems = currentTodo;
+  if (todoaction.type === "ADDTODO") {
+    newTodoItems = [
+      ...currentTodo,
+      {
+        itemName: todoaction.payload.itemName,
+        itemDate: todoaction.payload.itemDate,
+      },
+    ];
+  } else if (todoaction.type === "DELETETODO") {
+    newTodoItems = currentTodo.filter(
+      (item) => item.itemName !== todoaction.payload.itemName
+    );
+  }
+  return newTodoItems;
+}
 
 function App() {
+  const [todoItems, todoItemsdispatcher] = useReducer(todoReducer, []);
+
+  function addTodo(itemName, itemDate) {
+    const todoaction = {
+      type : "ADDTODO",
+      payload: {
+        itemName,
+        itemDate,
+      },
+    };
+    todoItemsdispatcher(todoaction);
+  }
+  function deleteTodo(itemName) {
+    const todoaction = {
+      type : "DELETETODO",
+      payload: {
+        itemName,
+      },
+    };
+    todoItemsdispatcher(todoaction);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="mainContainer">
+        <MyContext.Provider value={{ todoItems, addTodo, deleteTodo }}>
+          <Heading></Heading>
+          <Inputodo></Inputodo>
+          <TodoItems></TodoItems>
+        </MyContext.Provider>
+      </div>
+    </>
   );
 }
 
